1a740346dc6dd33ac904720db14ecb7d04b7d842
JonasHeselschwerdt <jonas.heselschwerdt@t-online.de>
2025-05-02 11:56:44 +0200

    First try with UDP

--- client.py
+++ client.py
@@ -10,9 +10,13 @@ window = pygame.display.set_mode((s.WIDTH, s.HEIGHT))
 pygame.display.set_caption("Space Invader PVP") 
 clock = pygame.time.Clock() 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)            
-sock.connect(('192.168.0.133', 65432))   # hier die IPv4 des PIs eingeben
-own_ip = sock.getsockname()[0]
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+
+server_addr = ('192.168.178.53', 65432)
+own_ip = socket.gethostbyname(socket.gethostname())
+
+#sock.connect(('192.168.178.53', 65432))   # hier die IPv4 des PIs eingeben
+#own_ip = sock.getsockname()[0]
 
 running = True
 
@@ -42,12 +46,19 @@ while running:
     input = {"MouseX": mouse_x , "MouseY": mouse_y , "shoot": shoot}
 
     data = pickle.dumps(input)
-    sock.sendall(len(data).to_bytes(4, 'big') + data)
+    #sock.sendall(len(data).to_bytes(4, 'big') + data)
+    sock.sendto(data, server_addr)
 
     # --- Daten vom Server empfangen ---
     try:
         # Erst die Länge lesen
-        length_bytes = sock.recv(4)
+        sock.settimeout(0.05)  # kurz warten
+        try:
+            data, _ = sock.recvfrom(4096)
+            player_info = pickle.loads(data)
+        except socket.timeout:
+            continue  # falls kein Paket empfangen wurde
+        """length_bytes = sock.recv(4)
         if not length_bytes:
             break  # Verbindung verloren
         length = int.from_bytes(length_bytes, 'big')
@@ -60,19 +71,19 @@ while running:
                 break
             received_data += packet
 
-        player_info = pickle.loads(received_data)
+        player_info = pickle.loads(received_data)"""
 
         if own_ip == player_info["PlayerIPs"][0]:
             Localplayercolour = player_info["colour1"]
             Localplayerx = player_info["x1"]
             Localplayery = player_info["y1"]
-            Enemyplayercolour = player_info["colour2"]
-            Enemyplayerx = player_info["x2"]
-            Enemyplayery = player_info["y2"]
+            #Enemyplayercolour = player_info["colour2"]
+            #Enemyplayerx = player_info["x2"]
+            #Enemyplayery = player_info["y2"]
         else:
-            Localplayercolour = player_info["colour2"]
-            Localplayerx = player_info["x2"]
-            Localplayery = player_info["y2"]
+            #Localplayercolour = player_info["colour2"]
+            #Localplayerx = player_info["x2"]
+            #Localplayery = player_info["y2"]
             Enemyplayercolour = player_info["colour1"]
             Enemyplayerx = player_info["x1"]
             Enemyplayery = player_info["y1"]
@@ -82,7 +93,7 @@ while running:
         running = False
 
     pygame.draw.rect(window, Localplayercolour, (Localplayerx - 20, Localplayery - 10, 40, 20))
-    pygame.draw.rect(window, Enemyplayercolour, (Enemyplayerx - 20, Enemyplayery - 10, 40, 20))
+    #pygame.draw.rect(window, Enemyplayercolour, (Enemyplayerx - 20, Enemyplayery - 10, 40, 20))
 
     pygame.display.flip()
 
--- server.py
+++ server.py
@@ -8,7 +8,7 @@ import time
 HOST = '0.0.0.0'  
 PORT = 65432     
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind((HOST, PORT))
 
 connections = []
@@ -37,10 +37,10 @@ def client_thread(conn, spieler_id):
     finally:
         conn.close()
 
-server_socket.listen(2)  
+#server_socket.listen(2)  
 print(f"Server läuft auf {HOST}:{PORT} und wartet auf Verbindung...")
 
-playercounter = 0
+"""playercounter = 0
 while playercounter < 1:
 
     conn, addr = server_socket.accept()
@@ -50,42 +50,59 @@ while playercounter < 1:
     thread = threading.Thread(target=client_thread, args=(conn, playercounter))
     thread.start()
     playercounter += 1
+"""
+player_addresses = {}
+playerinput = {}
 
 Player1 = Player((s.WIDTH // 2, s.HEIGHT - 40),s.BLUE)
-Player2 = Player((s.WIDTH // 2 , 40),s.RED)
+#Player2 = Player((s.WIDTH // 2 , 40),s.RED)
 
 while True:
 
     start_time = time.time()
+    # UDP-Daten empfangen
+    try:
+        server_socket.settimeout(0.01)
+        while True:
+            data, addr = server_socket.recvfrom(4096)
+            input_data = pickle.loads(data)
+
+            if addr not in player_addresses.values():
+                player_id = len(player_addresses)
+                player_addresses[player_id] = addr
+                print(f"Spieler {player_id} verbunden: {addr}")
+
+            player_id = [k for k, v in player_addresses.items() if v == addr][0]
+            playerinput[player_id] = input_data
+    except socket.timeout:
+        pass
 
     try:
         player1move = (playerinput[0]["MouseX"], playerinput[0]["MouseY"])
-        player2move = (playerinput[1]["MouseX"], playerinput[1]["MouseY"])
+        #player2move = (playerinput[1]["MouseX"], playerinput[1]["MouseY"])
     except:
         player1move = (0,0)
-        player2move = (0,0)
+        #player2move = (0,0)
 
     Player1.move(player1move)
-    Player2.move(player2move)
+    #Player2.move(player2move)
 
     daten = {
         "x1": Player1.x,
         "y1": Player1.y,
         "colour1": Player1.colour,
-        "x2": Player2.x,
-        "y2": Player2.y,
-        "colour2":Player2.colour,
+        #"x2": Player2.x,
+        #"y2": Player2.y,
+        #"colour2":Player2.colour,
         "PlayerIPs": player_ips
     }
 
     data_serialized = pickle.dumps(daten)
     data_length = len(data_serialized).to_bytes(4, 'big')
 
-    for conn in connections:
-        try:
-            conn.sendall(data_length + data_serialized)
-        except:
-            pass
+    for addr in player_addresses.values():
+        server_socket.sendto(data_serialized, addr)
+
 
     elapsed_time = time.time() - start_time
     time_to_sleep = max(0,1/60 - elapsed_time)
